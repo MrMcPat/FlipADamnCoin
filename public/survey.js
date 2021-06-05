8c9e99957ccbb9cecce796721323693a6ae6245c
unknown <pl204511194@yahoo.com>
2021-06-05 19:27:32 -0400

    minor bug fixes

--- public/survey.js
+++ public/survey.js
@@ -1,11 +1,6 @@
+$('#surveyUndecided').prop("checked", true);
 $('.survey-button').on('click', function() {
   if ($('#additional-comments').val().length === 0) {
     $('#additional-comments').text("No comment.");
   } else {}
-});
-
-$('.survey-button').on('click', function() {
-  if ($('#surveyYes').prop("checked", false) && $('#surveyNo').prop("checked", false) && $('#surveyUndecided').prop("checked", false)) {
-    $('#surveyUndecided').prop("checked", true);
-  }
 });
