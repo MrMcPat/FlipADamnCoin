978d0994617a4077fd9f31cc9be909f07e38aa78
unknown <pl204511194@yahoo.com>
2021-06-04 23:57:07 -0400

    added database

--- app.js
+++ app.js
@@ -5,12 +5,17 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.set('view engine', 'ejs');
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/coinflipDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://admin-pat:789632145@cluster0.smilb.mongodb.net/coinflipDB?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 mongoose.set('useFindAndModify', false);
 
 let postOutcome;
@@ -25,11 +30,11 @@ const coinFlipSchema = {
 
 const CoinFlip = mongoose.model("CoinFlip", coinFlipSchema);
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
   res.render("index");
 });
 
-app.post("/", function(req, res){
+app.post("/", function(req, res) {
   const possibleOutcome = {
     heads: req.body.inputHeads,
     tails: req.body.inputTails
@@ -37,36 +42,36 @@ app.post("/", function(req, res){
   postOutcome = possibleOutcome;
 });
 
-app.get("/oneflip", function(req, res){
-  res.render("oneflip" , {
+app.get("/oneflip", function(req, res) {
+  res.render("oneflip", {
     resultHeads: postOutcome.heads,
     resultTails: postOutcome.tails
   });
 });
 
-app.get("/2-3flips", function(req, res){
-  res.render("2-3flips" , {
+app.get("/2-3flips", function(req, res) {
+  res.render("2-3flips", {
     resultHeads: postOutcome.heads,
     resultTails: postOutcome.tails
   });
 });
 
-app.get("/3-5flips", function(req, res){
+app.get("/3-5flips", function(req, res) {
   res.render("3-5flips", {
     resultHeads: postOutcome.heads,
     resultTails: postOutcome.tails
   });
 });
 
-app.get("/about", function(req, res){
+app.get("/about", function(req, res) {
   res.render("about");
 });
 
-app.get("/survey", function(req, res){
+app.get("/survey", function(req, res) {
   res.render("survey");
 });
 
-app.post("/survey", function(req, res){
+app.post("/survey", function(req, res) {
   postOutcome = new CoinFlip({
     headsInput: postOutcome.heads,
     tailsInput: postOutcome.tails,
@@ -78,18 +83,18 @@ app.post("/survey", function(req, res){
   res.redirect("/history");
 });
 
-app.get("/history", function(req, res){
-  CoinFlip.find({}, function(err, coinflips){
+app.get("/history", function(req, res) {
+  CoinFlip.find({}, function(err, coinflips) {
     res.render("history", {
       results: coinflips
     });
   });
 });
 
-app.post("/delete", function(req, res){
+app.post("/delete", function(req, res) {
   const coinFlipId = req.body.delete;
 
-  CoinFlip.findByIdAndRemove(coinFlipId, function(err){
+  CoinFlip.findByIdAndRemove(coinFlipId, function(err) {
     res.redirect("/history");
   });
 });
